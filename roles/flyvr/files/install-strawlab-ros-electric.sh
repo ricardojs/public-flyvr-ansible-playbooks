4effa67e36436370419288d7187218592ccfcb4a
Andrew Straw <strawman@astraw.com>
2013-07-11 09:53:42 +0200

    initial playbook for public FlyVR install

---
+++ roles/flyvr/files/install-strawlab-ros-electric.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+set -e
+
+export ROS_TARGET="/opt/ros/ros.electric.boost1.46"
+export FLYCAVE_TARGET="/opt/ros/ros-flycave.electric.boost1.46"
+export ROSINSTALL_POSTFIX="-git"
+
+wget https://raw.github.com/strawlab/rosinstall/master/scripts/electric_check_ros.bash -O /tmp/electric_check_ros.bash
+chmod a+x /tmp/electric_check_ros.bash
+/tmp/electric_check_ros.bash
+
+
+wget https://raw.github.com/strawlab/rosinstall/master/scripts/electric_check_flyvr.bash -O /tmp/electric_check_flyvr.bash
+chmod a+x /tmp/electric_check_flyvr.bash
+/tmp/electric_check_flyvr.bash
